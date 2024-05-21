6b98e0268c9b0f831e4abe06f748dcc57bc604af
Tsurematsu <daniel120802@gmail.com>
2024-05-21 03:34:13 -0500

    arreglo certificados

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+import fs from 'fs';
+import https from 'https';
+import http from 'http';
+import cors from 'cors'; // Importar el paquete cors
 import express from 'express';
 import path from 'path';
 import productController from './Backend/src/controllers/productController.js';
 import db from "./Backend/db/db.js"
+const Origins = process.env.ORIGINS || '*';
 let dbProductos = new db("./Backend/db/productos.json")
 let dbUsuarios = new db("./Backend/db/usuarios.json")
 const app = express();
-const PORT = 3000;
 
 // Define la ruta de la carpeta estática ////ruta relativa
 const staticFolder = path.resolve('./FrontEnd/public');
@@ -19,10 +23,12 @@ app.use("/uploads",express.static(uploadFolder));
 app.use(express.json()); // app.use(express.static('build'));
 productController(app,dbProductos,dbUsuarios)
 
-
-
-// Inicia el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor Express escuchando en el puerto http://localhost:${PORT}`);
-});
-
+const PORT = 3080;
+const PORTssl = 3443;
+const privateKey = fs.readFileSync('./cert/private.key', 'utf8');
+const certificate = fs.readFileSync('./cert/certificate.crt', 'utf8');
+const certificateCA = fs.readFileSync('./cert/ca_bundle.crt', 'utf8');
+const serverHTTPS = https.createServer({ key: privateKey, cert: certificate, ca: certificateCA}, app);
+const serverHTTP = http.createServer(app);
+serverHTTPS.listen(PORTssl, () => {console.log(`Servidor HTTPS ${PORTssl}`);});
+serverHTTP.listen(PORT, () => {console.log(`Servidor HTTP ${PORT}`);});
